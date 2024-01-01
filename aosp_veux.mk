eef98cc3aa6bda635d02fa8f7917e6187d8a1c99
MrTopia <redkarmakar@gmail.com>
2024-01-01 23:19:12 +0530

    veux : Welcome FireDroid

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -12,25 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Matrixx stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FireDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Matrixx 
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := AswinOP
-MATRIXX_CHIPSET := SM6375
-MATRIXX_BATTERY := 5000mah
-MATRIXX_DISPLAY := 1080X2400
+# Rom Specific Flags
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
 
-# Gapps
-WITH_GMS := true
-
+# FireDroid Maintainer Flags
+FIREDROID_MAINTAINER := MrTopiA
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 # AOSP Recovery
 TARGET_USES_AOSP_RECOVERY := true
 
 # BOOT_ANIMATION
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Nuke AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
@@ -39,7 +36,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 TARGET_ENABLE_BLUR := true	
 
 # Device identifier
-PRODUCT_NAME := lineage_veux
+PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
